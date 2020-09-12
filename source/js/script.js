f29dd9f2992998f9247cae00aac1a1370762dafe
Roman Pogorelov <rmk140987@gmail.com>
2020-09-12 16:40:03 +0300

    5. Added overlay layout & modal popup window.

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,23 @@ toggleButton.onclick = () => {
   menuWithCatalog.classList.toggle("menu--hidden");
   menuWithSearch.classList.toggle("menu--hidden");
 }
+
+let overlay = document.querySelector(".overlay");
+let modal = document.querySelector(".modal");
+let orderButton = document.querySelector(".week-product__order-button");
+let cardOrderButtons = document.querySelectorAll(".card__order-button");
+
+function overlayShow(evt) {
+  evt.preventDefault();
+  overlay.classList.toggle("overlay--close");
+  modal.classList.toggle("modal--close");
+}
+
+if (orderButton) {
+  orderButton.onclick = overlayShow;
+} else if (cardOrderButtons.length > 0) {
+  cardOrderButtons.forEach(
+    element => element.onclick = overlayShow);
+}
+
+overlay.onclick = overlayShow;
